9a27e9d328c7af76c9d282854eaa79dfc6cc84fb
rounderjd <81671585+rounderjd@users.noreply.github.com>
2025-05-13 10:25:18 -0500

    Add files via upload

--- loki-ts/entrypoint.sh
+++ loki-ts/entrypoint.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-# Load TS_AUTHKEY from Docker secret
 export TS_AUTHKEY=$(cat /run/secrets/TS_AUTHKEY)
 
-# Start Tailscale
+# Start Tailscale with SOCKS5 proxy
 tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 sleep 3
 
+# DNS proxy to forward DNS queries to MagicDNS
+socat UDP-LISTEN:53,fork UDP:100.100.100.100:53 &
+
+# Configure DNS for tailnet hostname resolution
+echo "nameserver 127.0.0.1" > /etc/resolv.conf
+echo "search narwhal-halibut.ts.net" >> /etc/resolv.conf
+
+# Bring up Tailscale
 tailscale up --authkey="${TS_AUTHKEY}" \
   --hostname=loki \
-  --accept-routes=false \
+  --accept-routes=true \
+  --accept-dns=false \
   --ssh \
   --ephemeral \
-  --reset \
-  --hostname=loki.narwhal-halibut.ts.net
+  --reset
 
-# Start service
+# Start main service
 exec /usr/bin/loki -config.file=/etc/loki/local-config.yaml
